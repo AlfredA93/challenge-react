ba65ba8e522eb470247ff7029959c14aeb5982dc
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-05 10:44:27 +0200

    Add ES7Snippets file for reference

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,36 @@
+// rfce
+// import React, { Component } from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// rafce
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// };
+
+// rce
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   };
+// };
+
+// rconst
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// };
+
+// clg
+// console.log(first)
